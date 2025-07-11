41056696d42638ba5b751975ff0103507867392e
ganzhonghuan <0newryangun@google.com>
2025-07-11 13:39:17 +0800

    feat: update style

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const withBundleAnalyzer = bundleAnalyzer({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
